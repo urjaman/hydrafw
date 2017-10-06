a9b5160b2d2c783b9592e78d9f5c0ad39a5da437
Urja Rannikko <urjaman@gmail.com>
2017-10-06 14:14:40 +0300

    Hook the bsp_spi API to frser
    I think i might've slightly abused how it is meant to be initialized,
    but that is with questions (= this is to highlight what I'm confused about).
    Anyways it works to probe an SPI chip with flashrom.
    I didnt test the speed setting code yet.

--- src/frser/hydrafw-frser.c
+++ src/frser/hydrafw-frser.c
@@ -20,6 +20,7 @@
 #include "frser.h"
 #include "frser-flashapi.h"
 #include "uart.h"
+#include "bsp_spi.h"
 
 /* frser I/O functions need to know where to put the data, and frser was not built for multi-instance... */
 static t_hydra_console *frser_con = 0;
@@ -61,25 +62,65 @@ void run_frser(t_hydra_console *con, int via_sync)
 	frser_con = 0;
 }
 
+
+/* Why does this kind of a structure exist somewhere down in the console API? The duck does the console have
+ * to do with what other interfaces the device happens to be using?. WTF. And why does this structure contain
+ * huge communication buffers? WTF^2. */
+static uint8_t spi_initialized = 0;
+static mode_config_proto_t frser_spi_cfg = {
+	.dev_num = BSP_DEV_SPI1,
+	.dev_gpio_pull = MODE_CONFIG_DEV_GPIO_NOPULL,
+	.dev_mode = DEV_SPI_MASTER,
+	.dev_bit_lsb_msb = DEV_SPI_FIRSTBIT_MSB
+};
+
 void flash_select_protocol(uint8_t allowed_protocols)
 {
 	(void)allowed_protocols;
+	/* This API is redundant. WTF^3. */
+	bsp_spi_init(frser_spi_cfg.dev_num, &frser_spi_cfg);
+	spi_initialized = 1;
 }
 
 void flash_spiop(uint32_t sbytes, uint32_t rbytes)
 {
-	while (sbytes--) RECEIVE();
+	/* This API is synchronous, so it'll never reach the requested SPI speed, but there
+	 * will be gaps between the bytes on the SPI interface. With 168 Mhz the gaps
+	 * might be very small, but anyways... */
+	bsp_spi_select(frser_spi_cfg.dev_num);
+	while (sbytes--) {
+		uint8_t d = RECEIVE();
+		bsp_spi_write_u8(frser_spi_cfg.dev_num, &d, 1);
+	}
 	SEND(S_ACK);
-	while (rbytes--) SEND(0xFF);
+	while (rbytes--) {
+		uint8_t d;
+		bsp_spi_read_u8(frser_spi_cfg.dev_num, &d, 1);
+		SEND(d);
+	}
+	bsp_spi_unselect(frser_spi_cfg.dev_num);
 }
 
 uint32_t spi_set_speed(uint32_t hz)
 {
-	return hz;
+	uint32_t chz = 42000000UL; /* FIXME: get fPCLK from somewhere, divide that by 2 */
+	uint8_t dsel = 0;
+	do {
+		if (hz >= chz) break;
+		chz /= 2;
+		dsel++;
+	} while (dsel<7);
+	/* The dev_speed parameter is inverted from what hardware uses (this division selector), so we
+	 * just calculated what the hardware would use, and then we invert it to use this API... */
+	frser_spi_cfg.dev_speed = 7 - dsel;
+	/* If it was already initialized, re-initialize with the new speed. */
+	if (spi_initialized) bsp_spi_init(frser_spi_cfg.dev_num, &frser_spi_cfg);
+	return chz;
 }
 
 void flash_set_safe(void)
 {
-
+	bsp_spi_deinit(frser_spi_cfg.dev_num);
+	spi_initialized = 0;
 }
 
